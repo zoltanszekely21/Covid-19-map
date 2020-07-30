2059abc06e2ef2212a89943bb6b4001f68420507
Zoltan-Ioan Szekely <45119303+zoltanszekely21@users.noreply.github.com>
2020-07-30 05:57:18 +0300

    Update map-services.js

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -8,6 +8,6 @@ export const mapServices = [
   {
     name: 'Mapbox',
     attribution: 'Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>',
-    url: '<Insert personal Mapbox API>'
+    url: 'https://api.mapbox.com/styles/v1/{username}/{style_id}/tiles/{tilesize)/{z}/{x}/{y}@2x?access_token={accessToken}'
   }
 ];
