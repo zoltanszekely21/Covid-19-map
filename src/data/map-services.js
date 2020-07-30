ff994c5659f4085990df020abdf7268f0fb5b746
zoltanszekely21 <zoltan.szekely21@gmail.com>
2020-07-30 05:41:03 +0300

    File update

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -8,6 +8,6 @@ export const mapServices = [
   {
     name: 'Mapbox',
     attribution: 'Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>',
-    url: 'https://api.mapbox.com/styles/v1/zltnck/ckd3pmzuu0ann1irphdmpiycq/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoiemx0bmNrIiwiYSI6ImNrZDNwb2N1bzFwcGkydHBndWh4Mmg2NGMifQ.Tm3EU4_5iSfVtZukxwKjhw'
+    url: '<Insert personal Mapbox API>'
   }
 ];
